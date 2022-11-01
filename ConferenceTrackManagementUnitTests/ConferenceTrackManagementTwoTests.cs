ce12c58fab9e44d04cd2950228d2bb97e270bcf3
alix <alix.klingenberg@octopus.com>
2022-11-02 10:26:39 +1300

    day 2 : part 1

---
+++ ConferenceTrackManagementUnitTests/ConferenceTrackManagementTwoTests.cs
@@ -0,0 +1,15 @@
+namespace ConferenceTrackManagementUnitTests;
+
+public class ConferenceTrackManagementTwoTests
+{
+    // Parse talks from input
+    [Fact]
+    public void GivenSingleTalkCanParseTitleAndTime()
+    {
+        
+    }
+    
+    // Arranges talks into sessions
+    
+    // Pretty prints the output
+}
