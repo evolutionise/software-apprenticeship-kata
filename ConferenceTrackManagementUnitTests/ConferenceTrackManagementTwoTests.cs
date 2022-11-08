f0eecdab6d30f5a6b316912c4cee9950d82e590d
alix <alix.klingenberg@octopus.com>
2022-11-09 10:28:24 +1300

    D3P2

--- ConferenceTrackManagementUnitTests/ConferenceTrackManagementTwoTests.cs
+++ ConferenceTrackManagementUnitTests/ConferenceTrackManagementTwoTests.cs
@@ -33,13 +33,35 @@ public class InputParser
 
         var matches = regex.Matches(input);
 
-        var titleWordsWithoutTime = input.Split(' ').TakeWhile(w => w != matches[0].Value);
+        var titleWordsWithoutTime = input.Split(' ').TakeWhile(w => w != matches[0].Value + "min");
         var titleWithoutTime = string.Join(' ', titleWordsWithoutTime);
 
         var time = matches[0].Value.Substring(0, matches[0].Length - 3);
 
-        return new Talk(titleWithoutTime, time);
+        var timespan = new TimeSpan(0, minutes: int.Parse(time), 0);
+
+        return new Talk(titleWithoutTime, timespan);
     }
 }
 
 public record Talk(string Title, TimeSpan Length);
+
+// Writing Fast Tests Against Enterprise Rails 60min  
+// Overdoing it in Python 45min  
+// Lua for the Masses 30min  
+// Ruby Errors from Mismatched Gem Versions 45min   
+// Common Ruby Errors 45min  
+// Rails for Python Developers lightning  
+//     Communicating Over Distance 60min  
+// Accounting-Driven Development 45min  
+// Woah 30min  
+// Sit Down and Write 30min  
+// Pair Programming vs Noise 45min  
+// Rails Magic 60min  
+// Ruby on Rails: Why We Should Move On 60min  
+// Clojure Ate Scala (on my project) 45min  
+// Programming in the Boondocks of Seattle 30min  
+// Ruby vs. Clojure for Back-End Development 30min  
+// Ruby on Rails Legacy App Maintenance 60min  
+// A World Without HackerNews 30min  
+// User Interface CSS in Rails Apps 30min  
