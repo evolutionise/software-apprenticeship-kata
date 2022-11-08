345ce60456f6364b048cfc1c30780b4e18698dfa
alix <alix.klingenberg@octopus.com>
2022-11-09 10:40:43 +1300

    Day 3 WIP 3

--- kata/InputParser.cs
+++ kata/InputParser.cs
@@ -4,19 +4,35 @@ namespace kata;
 
 public class InputParser
 {
-    public Talk Parse(string input)
+    public Talk Parse(string rawTitleString)
+    {
+        var timeMatch = MatchOnTime(rawTitleString);
+        var titleWithoutTime = ExtractTitleFromRawString(rawTitleString, timeMatch);
+        var timespan = GetTimeFromMatch(timeMatch);
+
+        return new Talk(titleWithoutTime, timespan);
+    }
+
+    private static TimeSpan GetTimeFromMatch(Match timeMatch)
+    {
+        var time = timeMatch.Value[..2];
+
+        var timespan = new TimeSpan(0, minutes: int.Parse(time), 0);
+        return timespan;
+    }
+
+    private static Match MatchOnTime(string input)
     {
         var regex = new Regex(@"(\w+)(\d+)");
 
         var matches = regex.Matches(input);
+        return matches[0];
+    }
 
-        var titleWordsWithoutTime = input.Split(' ').TakeWhile(w => w != matches[0].Value + "min");
+    private static string ExtractTitleFromRawString(string input, Match matches)
+    {
+        var titleWordsWithoutTime = input.Split(' ').TakeWhile(w => w != matches.Value + "min");
         var titleWithoutTime = string.Join(' ', titleWordsWithoutTime);
-
-        var time = matches[0].Value.Substring(0, 2);
-
-        var timespan = new TimeSpan(0, minutes: int.Parse(time), 0);
-
-        return new Talk(titleWithoutTime, timespan);
+        return titleWithoutTime;
     }
 }
