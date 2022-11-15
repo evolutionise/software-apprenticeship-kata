66259a5c6a767c3fcc640b00d1b7e857d3ce24b7
alix <alix.klingenberg@octopus.com>
2022-11-16 10:40:18 +1300

    WIP 2

--- kata/ConferenceTrackManagementTwo/ConferenceTrack.cs
+++ kata/ConferenceTrackManagementTwo/ConferenceTrack.cs
@@ -4,6 +4,25 @@ public class ConferenceTrack
 {
     public ConferenceTrack(List<ConferenceTalk> talks)
     {
+        var timespan = new TimeSpan(0);
+        var seed = new List<(TimeSpan, List<ConferenceTalk>)>
+        {
+            (TimeSpan.Zero, new List<ConferenceTalk>())
+        };
+        
+        var talksTotallingLessThan4Hrs = talks.Aggregate(
+            seed,
+            (accumulatedListOfTalks, talk) =>
+            {
+                var lastAddedItem = accumulatedListOfTalks.Last();
+                lastAddedItem.Item2.Add(talk);
+                accumulatedListOfTalks.Add(
+                    (lastAddedItem.Item1.Add(new TimeSpan(0, talk.Minutes.Value, 0)), lastAddedItem.Item2));
+                return accumulatedListOfTalks;
+            });
+        
+        
+        MorningSession = new ConferenceSession(talks);
     }
 
     public ConferenceSession MorningSession { get; set; }
